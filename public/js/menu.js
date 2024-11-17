f736c8317956b53e852b6b59f05bdc06ff288ec4
agvgdev <agvgdev@gmail.com>
2024-11-17 12:36:23 -0300

    modificações na estilização

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,4 +1,8 @@
-function toggleMenu() {
-    const navMenu = document.querySelector('.nav-menu');
-    navMenu.classList.toggle('active');
-}
+document.addEventListener('DOMContentLoaded', function () {
+    const menuToggle = document.getElementById('menu-toggle');
+    const menu = document.getElementById('nav-menu');
+
+    menuToggle.addEventListener('click', function () {
+        menu.classList.toggle('active'); // Adiciona ou remove a classe 'active'
+    });
+});
